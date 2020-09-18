8d62c585fd5fe25d6d08069d6ff8a4e1a04d8b9c
play.js <git@gituser.com>
2020-09-18 07:30:46 -0400

    added the item table component

---
+++ src/components/ItemTable.vue
@@ -0,0 +1,43 @@
+<template>
+   <table>
+      <thead>
+        <tr>
+          <th>Quantity</th>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items">
+          <td>{{item.quantity}}</td>
+          <td>{{item.name}}</td>
+          <td>{{item.price}}</td>
+        </tr>
+      </tbody>
+    </table>
+</template>
+
+
+<script>
+export default {
+  name: 'ItemTable',
+  data () {
+    return {
+      items: [
+        {
+          id: 1, quantity: 1, 
+          name: 'banana', price: .76
+        },
+        {
+          id: 1, quantity: 3, 
+          name: 'apple', price: 1.99
+        },
+        {
+          id: 1, quantity: 5, 
+          name: 'orange', price: 2.99
+        }
+      ]
+    }
+  }
+}
+</script>
